ddbf7596980b7606da386ea23cc7bdee31572ae9
GhostCh17 <jhonchistama00617@gmail.com>
2023-04-13 20:53:14 -0500

    Se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//EJERCICIO NÚMERO 01
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Aplicación para escribir tu nombre y dar la hora actual");
+Console.Write("Ingresa tu nombre: ");
+String name = Console.ReadLine();
+Console.WriteLine("Hola " +  name);
+
+//EJERCICIO NÚMERO 02
+DateTime Hora = DateTime.Now;
+Console.WriteLine("La hora es " + Hora.ToString("HH:mm:ss"));
